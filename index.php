875ff941000da264aa09ac6dc3fa37037a39d96a
Davidre0795 <32368313+Davidre0795@users.noreply.github.com>
2019-03-14 11:43:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo "hola";
   
 require_once "controladores/plantilla.controlador.php";
 require_once "controladores/productos.controlador.php";
@@ -23,4 +22,7 @@ require_once "extensiones/PHPMailer/PHPMailerAutoload.php";
 require_once "extensiones/vendor/autoload.php";
 
 $plantilla = new ControladorPlantilla();
+
+echo $plantilla;
+
 $plantilla -> plantilla();
