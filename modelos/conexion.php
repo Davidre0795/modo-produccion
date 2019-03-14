062cfbf3bf0e084e57ca87e3a2b2a069571213a3
Davidre0795 <32368313+Davidre0795@users.noreply.github.com>
2019-03-13 21:17:47 -0600

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -5,8 +5,8 @@ class Conexion{
 	public function conectar(){
 
 		$link = new PDO("mysql:host=35.196.228.81;dbname=ecommerce",
-						"usuario",
-						"contraseña",
+						"prueba",
+						"admin123",
 						array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		                      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 						);
