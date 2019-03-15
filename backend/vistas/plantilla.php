a3fafb45e046feb43141123bf4a59ebc952a8bcf
Davidre0795 <32368313+Davidre0795@users.noreply.github.com>
2019-03-15 13:44:44 -0600

    Update plantilla.php

--- backend/vistas/plantilla.php
+++ backend/vistas/plantilla.php
@@ -106,7 +106,7 @@ session_start();
   <script src="vistas/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 
   <!-- ChartJS -->
-  <script src="vistas/bower_components/Chart.js/Chart.js"></script>
+  <script src="vistas/bower_components/chart.js/Chart.js"></script>
 
   <!-- SweetAlert 2 https://sweetalert2.github.io/-->
   <script src="vistas/plugins/sweetalert2/sweetalert2.all.js"></script>
