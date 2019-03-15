3591b679354c04859f16f52a20fc699d919ed181
Davidre0795 <32368313+Davidre0795@users.noreply.github.com>
2019-03-15 14:29:23 -0600

    Update conexion.php

--- backend/modelos/conexion.php
+++ backend/modelos/conexion.php
@@ -1,18 +1,12 @@
 <?php
-
 class Conexion{
-
 	public function conectar(){
-
-		$link = new PDO("mysql:host=127.0.0.1;dbname=ecommerce",
+		$link = new PDO("mysql:host=localhost;dbname=ecommerce",
 						"root",
 						"kpyM7ErFwbja",
 						array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		                      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 						);
-
 		return $link;
-
 	}
-
 }
