e0140edc8f487c8d5eb0752e4b58939d932d08e5
Davidre0795 <32368313+Davidre0795@users.noreply.github.com>
2019-03-15 10:55:31 -0600

    Update rutas.php

--- backend/modelos/rutas.php
+++ backend/modelos/rutas.php
@@ -4,14 +4,14 @@ class Ruta{
 	RUTA LADO DEL CLIENTE
 	=============================================*/	
 	public function ctrRuta(){
-		return "http://104.197.230.142/";
+		return "http://104.197.218.184/";
 	
 	}
 	/*=============================================
 	RUTA LADO DEL SERVIDOR
 	=============================================*/	
 	public function ctrRutaServidor(){
-		return "http://104.197.230.142/backend/";
+		return "http://104.197.218.184/backend/";
 	
 	}
 }
