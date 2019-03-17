361cc216db5c3fb60def0b204ca353b8a1c22e76
Davidre0795 <32368313+Davidre0795@users.noreply.github.com>
2019-03-17 09:37:54 -0600

    Update categorias.controlador.php

--- backend/controladores/categorias.controlador.php
+++ backend/controladores/categorias.controlador.php
@@ -94,7 +94,7 @@ class ControladorCategorias{
 				VALIDAR IMAGEN OFERTA
 				=============================================*/
 
-				$rutaOferta = "vistas/img/ofertas/default/default.jpg";
+				$rutaOferta = "vistas/img/ofertas/default/no-image.jpg";
 
 				if(isset($_FILES["fotoOferta"]["tmp_name"]) && !empty($_FILES["fotoOferta"]["tmp_name"])){
 
@@ -185,7 +185,7 @@ class ControladorCategorias{
 								   "oferta"=>0,
 								   "precioOferta"=>0,
 								   "descuentoOferta"=>0,
-								   "imgOferta"=>"vistas/img/ofertas/default/default.jpg",								   
+								   "imgOferta"=>"vistas/img/ofertas/default/no-image.jpg",								   
 								   "finOferta"=>"");
 
 
@@ -423,7 +423,7 @@ class ControladorCategorias{
 								   "oferta"=>0,
 								   "precioOferta"=>0,
 								   "descuentoOferta"=>0,
-								   "imgOferta"=>"vistas/img/ofertas/default/default.jpg",								   
+								   "imgOferta"=>"vistas/img/ofertas/default/no-image.jpg",								   
 								   "finOferta"=>"");
 
 					if($_POST["antiguaFotoOferta"] != ""){
