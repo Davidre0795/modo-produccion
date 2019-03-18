2bd4c062ec4c0f2d643cafc7339358df2aa6db20
Davidre0795 <32368313+Davidre0795@users.noreply.github.com>
2019-03-18 15:53:11 -0600

    Update categorias.controlador.php

--- backend/controladores/categorias.controlador.php
+++ backend/controladores/categorias.controlador.php
@@ -186,7 +186,7 @@ class ControladorCategorias{
 								   "precioOferta"=>0,
 								   "descuentoOferta"=>0,
 								   "imgOferta"=>"vistas/img/ofertas/default/default.jpg",								   
-								   "finOferta"=>"");
+								   "finOferta"=>"0000-00-00 00:00:00");
 
 
 				}
@@ -424,7 +424,7 @@ class ControladorCategorias{
 								   "precioOferta"=>0,
 								   "descuentoOferta"=>0,
 								   "imgOferta"=>"",								   
-								   "finOferta"=>"");
+								   "finOferta"=>"0000-00-00 00:00:00");
 
 					if($_POST["antiguaFotoOferta"] != ""){
 
