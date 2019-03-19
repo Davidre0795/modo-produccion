9351b2d16e66a5d2e766d8b5fc0674d648f5161c
Davidre0795 <32368313+Davidre0795@users.noreply.github.com>
2019-03-18 21:28:34 -0600

    Update productos.controlador.php

--- backend/controladores/productos.controlador.php
+++ backend/controladores/productos.controlador.php
@@ -390,8 +390,8 @@ class ControladorProductos{
 						   "oferta"=>0,
 						   "precioOferta"=>0,
 						   "descuentoOferta"=>0,
-						   "imgOferta"=>"",
-						   "finOferta"=>""
+						   "imgOferta"=>"vistas/img/ofertas/default/default.jpg",
+						   "finOferta"=>"1000-01-01 00:00:00"
 					   );
 
 				}
@@ -762,8 +762,8 @@ class ControladorProductos{
 								   "oferta"=>0,
 								   "precioOferta"=>0,
 								   "descuentoOferta"=>0,
-								   "imgOferta"=>"",								   
-								   "finOferta"=>""
+								   "imgOferta"=>"vistas/img/ofertas/default/default.jpg",								   
+								   "finOferta"=>"1000-01-01 00:00:00"
 								   );
 
 				}
