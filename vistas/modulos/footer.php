95db328c46cabbc9f7bd916bf6685df073326fad
Davidre0795 <32368313+Davidre0795@users.noreply.github.com>
2019-03-23 13:26:56 -0600

    Update footer.php

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -67,6 +67,11 @@ FOOTER
 			DATOS CONTACTO
 			======================================-->
 
+			<?php 
+
+			$datos_contacto = ControladorPlantilla::ctrEstiloPlantilla();
+
+			echo '
 			<div class="col-md-3 col-sm-6 col-xs-12 text-left infoContacto">
 				
 				<h5>Dudas e inquietudes, contáctenos en:</h5>
@@ -75,18 +80,18 @@ FOOTER
 				
 				<h5>
 					
-					<i class="fa fa-phone-square" aria-hidden="true"></i> (555) 555-55-55
+					<i class="fa fa-phone-square" aria-hidden="true"></i> '.$datos_contacto["phone_number"].'
 
 					<br><br>
 
-					<i class="fa fa-envelope" aria-hidden="true"></i> soporte@tiendavirtual.com
+					<i class="fa fa-envelope" aria-hidden="true"></i> '.$datos_contacto["email"].'
 
 					<br><br>
 
-					<i class="fa fa-map-marker" aria-hidden="true"></i> Calle 45F 82 - 31 Local 102
+					<i class="fa fa-map-marker" aria-hidden="true"></i> '.$datos_contacto["address"].'
 
 					<br><br>
-					Medellín | Colombia
+					'.$datos_contacto["city"].' | '.$datos_contacto["country"].'
 
 				</h5>
 
@@ -100,7 +105,7 @@ FOOTER
 
 			<div class="col-lg-4 col-md-3 col-sm-6 col-xs-12 formContacto">
 
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.061210342917!2d-75.60279258568647!3d6.255666795471985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e4429739f2122e9%3A0x4812b922f0ad8f18!2sCl.+45f+%2382-31%2C+Medell%C3%ADn%2C+Antioquia!5e0!3m2!1ses!2sco!4v1511900955540" width="100%" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>	
+				<iframe src="'.$datos_contacto["map"].'" width="100%" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>	
 				
 				<!-- <h4>RESUELVA SU INQUIETUD</h4> -->
 
@@ -130,6 +135,9 @@ FOOTER
 				?> -->
 
 			</div>
+			';
+			?>
+
 			
 		</div>
 
